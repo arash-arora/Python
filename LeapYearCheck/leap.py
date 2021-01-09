3a2ef01e1a3e0e7804518ab36ec1348099b19237
arash-arora <52750629+arash-arora@users.noreply.github.com>
2021-01-10 01:34:53 +0530

    Update leap.py

--- LeapYearCheck/leap.py
+++ LeapYearCheck/leap.py
@@ -1,1 +1,21 @@
+year = int(input("Which year do you want to check? "))
 
+# if year%400==0 or (year%4==0 and year%100!=0):
+#   print("Leap year.")
+
+# else:
+#   print("Not leap year.")
+
+
+#                 OR
+
+if year%4==0:
+  if year%100==0:
+    if year%400==0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
